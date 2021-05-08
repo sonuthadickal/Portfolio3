a1db62cf581687ca5781da26eb3d2e034a25316c
sonuthadickal <83806370+sonuthadickal@users.noreply.github.com>
2021-05-08 16:12:57 +0530

    updated

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -7,8 +7,8 @@ function Type() {
       options={{
         strings: [
           "Developer",
-          "Deep Learning Engineer",
-          "MERN Stack Developer",
+          "student",
+          "blogger",
           "Open Source Contributor",
         ],
         autoStart: true,
